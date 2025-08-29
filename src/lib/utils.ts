fdf5f5d8ccbcbbbc4cbe130a3362297535bf8d26
Businessagents <adhiabi@gmail.com>
2025-08-29 11:57:44 -0700

    delete

--- src/lib/utils.ts
+++
@@ -1,5 +0,0 @@
-// Utility function for combining class names
-// Since we're using Chakra UI instead of Tailwind, this is simplified
-export function cn(...inputs: (string | undefined | null | false)[]): string {
-  return inputs.filter(Boolean).join(' ')
-}
